9e0fe7261cde4d7bc050b19393c86603328d3c9b
Luke Taylor <luke@circleleasing.co.uk>
2023-12-11 23:05:29 +0000

    Use binary representation instead to count up to 31 instead of 5

--- 4/counter.py
+++ 4/counter.py
@@ -1,10 +1,19 @@
 from machine import Pin
 import time
 
+MAX_VAL = 31
+MIN_VAL = 0
+
 green_button = Pin(3, Pin.IN, Pin.PULL_DOWN)
 red_button = Pin(2, Pin.IN, Pin.PULL_DOWN)
 segments = [Pin(13, Pin.OUT), Pin(12, Pin.OUT), Pin(11, Pin.OUT), Pin(10, Pin.OUT), Pin(9, Pin.OUT)]
-count = -1
+count = MIN_VAL
+format_str = '{0:0' + str(len(segments)) + 'b}'
+
+def updateLights():
+    binary = list(format_str.format(count))
+    for i in range(len(segments)):
+        segments[i].value(1 if binary[i] == '1' else 0)
 
 #make sure all off
 for led in segments:
@@ -13,11 +22,17 @@ for led in segments:
 while True:
     time.sleep(0.1)
     
-    if green_button.value() == 1 and count < 4:
-        count = count + 1
-        segments[count].value(1)
+    if green_button.value() == 1:
+        if count < MAX_VAL:
+            count = count + 1
+        else:
+            count = MIN_VAL
+        updateLights()
         
     elif red_button.value() == 1:
-        segments[count].value(0)
-        if count >= 0:
+        if count > MIN_VAL:
             count = count - 1
+        else:
+            count = MAX_VAL
+        updateLights()
+            
