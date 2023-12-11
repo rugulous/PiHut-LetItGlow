9b4bbdcb4d9a43e111e17a6966bcac0d5bd77ba6
Luke Taylor <luke@circleleasing.co.uk>
2023-12-11 23:46:45 +0000

    Switch test - with light

--- 5/switch-checker.py
+++ 5/switch-checker.py
@@ -2,11 +2,14 @@ from machine import Pin
 import time
 
 dips = []
+segments = []
 for i in range(6, 1, -1):
     dips.append(Pin(i, Pin.IN, Pin.PULL_DOWN))
+    segments.append(Pin(i + 7, Pin.OUT))
     
 while True:
     for i in range(0, len(dips)):
+        segments[i].value(dips[i].value())
         if dips[i].value() == 1:
             print(f"Switch {i + 1} on")
         else:
