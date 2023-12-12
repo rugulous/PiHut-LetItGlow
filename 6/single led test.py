60e54c6731ef53eeefac76ee248e03fe5c12b4f2
Luke Taylor <luke@circleleasing.co.uk>
2023-12-12 23:05:13 +0000

    Fade over time

--- 6/single led test.py
+++ 6/single led test.py
@@ -1,6 +1,9 @@
 from machine import Pin
 from neopixel import NeoPixel
 import time
+import math
+
+TARGET_DUR = 2
 
 def adjust(curr, target):
     if(curr == target):
@@ -12,13 +15,21 @@ def adjust(curr, target):
         return curr + 1
 
 def fade(target_colour):
+    steps = [x - y for x, y in zip(target_colour, curr_colour)]
+    total_steps_needed = max([abs(s) for s in steps])
+    sleep_period = TARGET_DUR / total_steps_needed
+    print(f"Sleeping for {sleep_period}")
+    raw_val = [float(x) for x in curr_colour]
+    
     while target_colour != curr_colour:
         for i in range(3):
-            curr_colour[i] = adjust(curr_colour[i], target_colour[i])
+            if(curr_colour[i] != target_colour[i]):
+                raw_val[i] = raw_val[i] + (steps[i] / total_steps_needed)
+                curr_colour[i] = int(raw_val[i])
             
         grb_led.fill(curr_colour)
         grb_led.write()
-        time.sleep(0.01)
+        time.sleep(sleep_period)
 
 grb_led = NeoPixel(Pin(2), 1)
 curr_colour = list((0, 0, 0))
@@ -44,4 +55,4 @@ while True:
     for colour in colours:
         print(f"Changing to {colour}")
         fade(list(colours[colour]))
-        time.sleep(1)
+        #time.sleep(1)
