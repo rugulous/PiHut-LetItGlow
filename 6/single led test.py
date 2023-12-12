1e8c414e35481e85093a452ae207ed959f5010aa
Luke Taylor <luke@circleleasing.co.uk>
2023-12-12 22:37:40 +0000

    Loop through colours!

--- 6/single led test.py
+++ 6/single led test.py
@@ -1,7 +1,25 @@
 from machine import Pin
 from neopixel import NeoPixel
+import time
 
 grb_led = NeoPixel(Pin(2), 1)
 
-grb_led.fill((0, 0, 255))
-grb_led.write()
+colours = {
+    "white": (240, 140, 255),
+    "red": (0, 255, 0),
+    "green": (255, 0, 0),
+    "blue": (0, 0, 255),
+    "yellow": (255, 175, 150),
+    "orange": (238, 223, 105),
+    "pink": (150, 150, 200),
+    "purple": (40, 100, 255),
+    "ice_blue": (150, 25, 200),
+    "unicorn": (175, 150, 255),
+    "bogey": (215, 100, 0)
+}
+
+for colour in colours:
+    print(f"Changing to {colour}")
+    grb_led.fill(colours[colour])
+    grb_led.write()
+    time.sleep(1)
