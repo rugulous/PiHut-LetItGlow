c6ab5ff7d6fb1b06e7989bf52e10914f74f60f55
Luke Taylor <luke@circleleasing.co.uk>
2023-12-12 23:55:42 +0000

    Alternate lights

--- 7/slider controlled flashing.py
+++ 7/slider controlled flashing.py
@@ -6,13 +6,15 @@ import random
 slider = ADC(Pin(28))
 leds = [NeoPixel(Pin(2), 1), NeoPixel(Pin(5), 1)]
 flash = 0
+first = True
 
 while True:
     flash = slider.read_u16() / 65000
     
     colour = [random.randint(0, 255) for i in range(3)]
-    for led in leds:
-        led.fill(colour)
-        led.write()
+    led = leds[0 if first else 1]
+    led.fill(colour)
+    led.write()
+    first = not first
         
     time.sleep(flash)
