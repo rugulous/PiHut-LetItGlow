5076d0884076e7103cf1c43d7811a38299218850
Luke Taylor <luke@circleleasing.co.uk>
2023-12-12 22:49:30 +0000

    Now fade!

--- 6/single led test.py
+++ 6/single led test.py
@@ -2,7 +2,26 @@ from machine import Pin
 from neopixel import NeoPixel
 import time
 
+def adjust(curr, target):
+    if(curr == target):
+        return curr
+    
+    if(curr > target):
+        return curr - 1
+    else:
+        return curr + 1
+
+def fade(target_colour):
+    while target_colour != curr_colour:
+        for i in range(3):
+            curr_colour[i] = adjust(curr_colour[i], target_colour[i])
+            
+        grb_led.fill(curr_colour)
+        grb_led.write()
+        time.sleep(0.01)
+
 grb_led = NeoPixel(Pin(2), 1)
+curr_colour = list((0, 0, 0))
 
 colours = {
     "white": (240, 140, 255),
@@ -18,8 +37,11 @@ colours = {
     "bogey": (215, 100, 0)
 }
 
-for colour in colours:
-    print(f"Changing to {colour}")
-    grb_led.fill(colours[colour])
-    grb_led.write()
-    time.sleep(1)
+grb_led.fill(curr_colour)
+grb_led.write()
+
+while True:
+    for colour in colours:
+        print(f"Changing to {colour}")
+        fade(list(colours[colour]))
+        time.sleep(1)
