f9b0a38969e62c4459728ff3220740ef779b133a
Luke Taylor <luke@circleleasing.co.uk>
2023-12-20 20:13:48 +0000

    Animated Christmas

--- 10/christmas.py
+++ 10/christmas.py
@@ -1,5 +1,6 @@
 from machine import Pin
 from neopixel import NeoPixel
+import time
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -9,10 +10,17 @@ strand = NeoPixel(Pin(2), 15)
 strand.fill((0, 0, 0))
 strand.write()
 
-for led in range(len(strand)):
-    if led % 2 == 0:
-        strand[led] = GREEN
-    else:
-        strand[led] = RED
+colours = list((RED, GREEN))
+
+while True:
+    for led in range(len(strand)):
+        if led % 2 == 0:
+            strand[led] = colours[0]
+        else:
+            strand[led] = colours[1]
         
-strand.write()
+    strand.write()
+    
+    c = colours.pop(0)
+    colours.append(c)
+    time.sleep(0.25)
