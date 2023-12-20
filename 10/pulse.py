73079e011a26d660cdfd494304f16c6e021a640b
Luke Taylor <luke@circleleasing.co.uk>
2023-12-20 20:06:48 +0000

    Cooler pulse

--- 10/pulse.py
+++ 10/pulse.py
@@ -9,8 +9,18 @@ strand.write()
 time.sleep(1)
 
 while True:
-    for led in range(len(strand)):
-        for i in range(255, 10, -1):
-            strand[led] = (i, 0, 0)
-            strand.write()
-            time.sleep(0.001)
+    for i in range(3):
+        colour = list()
+        for n in range(3):
+            if n == i:
+                colour.append(10)
+            else:
+                colour.append(0)
+        
+    
+        for led in range(len(strand)):
+            for c in range(255, 10, -1):
+                colour[i] = c
+                strand[led] = colour
+                strand.write()
+                time.sleep(0.001)
