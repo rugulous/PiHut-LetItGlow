b8739f3dfcd04ae7539e22f9ee08f1fa9eae36a3
Luke Taylor <luke@circleleasing.co.uk>
2023-12-20 20:35:11 +0000

    Random slide colours

--- 10/slide up strip.py
+++ 10/slide up strip.py
@@ -1,27 +1,31 @@
 from machine import Pin, ADC
 from neopixel import NeoPixel
 import time
+import random
 
 LED_COUNT = 15
 MAX_VAL = 65535
 DIVISION = MAX_VAL / LED_COUNT
-STEP = round(255 / LED_COUNT)
-
-print(STEP)
+STEP = (LED_COUNT - 1) / 255
 
 strand = NeoPixel(Pin(2), LED_COUNT)
 slider = ADC(28)
 
 while True:
     reading = round(slider.read_u16() / DIVISION)
+    print(reading)
+    colour = [random.randint(100, 255) for i in range(3)]
     
     for i in range(reading):
-        val = round(STEP * (i + 1))
-        print(val)
-        strand[i] = (val, 0, 0)
+        mult = STEP * (i + 1)
+        print(mult)
+        strand[i] = (round(colour[0] * mult), round(colour[1] * mult), round(colour[2] * mult))
+        print(strand[i])
         
+    print()
+    
     for i in range(LED_COUNT - 1, reading, -1):
         strand[i] = (0, 0, 0)
     strand.write()
         
-    time.sleep(0.1)
+    time.sleep(1)
