1d01982317c8a3fc0c1645b868039d1b8b44a5fb
Luke Taylor <luke@circleleasing.co.uk>
2023-12-20 20:37:31 +0000

    Slightly easier to see with 255 colours?

--- 10/slide up strip.py
+++ 10/slide up strip.py
@@ -11,21 +11,21 @@ STEP = (LED_COUNT - 1) / 255
 strand = NeoPixel(Pin(2), LED_COUNT)
 slider = ADC(28)
 
+colours = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+
 while True:
     reading = round(slider.read_u16() / DIVISION)
     print(reading)
-    colour = [random.randint(100, 255) for i in range(3)]
+    colour = colours[random.randint(0, len(colours) - 1)]
     
     for i in range(reading):
         mult = STEP * (i + 1)
-        print(mult)
         strand[i] = (round(colour[0] * mult), round(colour[1] * mult), round(colour[2] * mult))
-        print(strand[i])
         
     print()
     
-    for i in range(LED_COUNT - 1, reading, -1):
+    for i in range(reading, LED_COUNT):
         strand[i] = (0, 0, 0)
     strand.write()
         
-    time.sleep(1)
+    time.sleep(0.1)
